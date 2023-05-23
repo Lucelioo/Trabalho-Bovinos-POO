8936c86628cf376d6584a0f6f2da7251cda57080
Lucélio <119617166+Lucelioo@users.noreply.github.com>
2023-05-23 16:59:58 -0300

    Update App.java

--- Trabalho Bovinos/Sistema de transporte/src/App.java
+++ Trabalho Bovinos/Sistema de transporte/src/App.java
@@ -3,12 +3,26 @@ import java.util.Random;
 public class App {
     public static void main(String[] args) {
         Random random = new Random();
-        
+
         Fazenda fazenda1[] = new Fazenda[50];
-        for(int i = 0; i<fazenda1.length;i++){
-            fazenda1[i].cadastrarBovino(random.nextInt(1000), random.nextDouble(500.00));
+        for(int i = 0; i<fazenda1.length; i++){
+            fazenda1[i] = new Fazenda();
+            fazenda1[i].cadastrarBovino(random.nextInt(900000)+100000, random.nextInt(341)+160);
         }
 
-        Fazenda fazenda2[] = new Fazenda[50];
+        double soma = 0;
+        for(int j = 0; j < fazenda1.length; j++) {
+            if(fazenda1[j] != null) {
+                soma += fazenda1[j].getBovino().getPeso();
+            }
+        }
+
+        System.out.println(soma);
+        double media = soma / fazenda1.length;
+        System.out.println(media);
+
+        Truck caminhao = new Truck();
+        caminhao.transporte(media);
+        System.out.println("a quantidade de vacas a serem transportadas e: " + caminhao.getCarregamento());
     }
 }
