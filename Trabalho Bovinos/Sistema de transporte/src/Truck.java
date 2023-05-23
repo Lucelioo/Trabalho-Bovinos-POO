a9ddcd6d503a6d64cb7c6c063d3dcfc712be888d
Lucélio <119617166+Lucelioo@users.noreply.github.com>
2023-05-23 17:01:11 -0300

    Update Truck.java

--- Trabalho Bovinos/Sistema de transporte/src/Truck.java
+++ Trabalho Bovinos/Sistema de transporte/src/Truck.java
@@ -1,3 +1,31 @@
 public class Truck {
-    
+    int carregamento;
+
+    public void transporte(double media){
+        if(media>=160 && media<=200){
+            carregamento = 40;
+        }
+        else if(media>=200 && media<=250){
+            carregamento = 35;
+        }
+        else if(media>=250 && media<=300){
+            carregamento = 30;
+        }
+        else if(media>=300 && media<=360){
+            carregamento = 25;
+        }
+        else if(media>=360 && media<=400){
+            carregamento = 20;
+        }
+        else if(media>=400 && media<=460){
+            carregamento = 18;
+        }
+        else if(media>=460 && media<=500){
+            carregamento = 16;
+        }
+    }
+
+    public int getCarregamento(){
+        return carregamento;
+    }
 }
