66f6d9d4c2d9c08ecd74b961ca111146117fcedc
Lucélio <119617166+Lucelioo@users.noreply.github.com>
2023-05-26 20:52:43 -0300

    Update App.java

--- Trabalho Bovinos/Sistema de transporte/src/App.java
+++ Trabalho Bovinos/Sistema de transporte/src/App.java
@@ -1,59 +1,142 @@
+import java.util.Scanner;
 import java.util.Random;
 
 public class App {
-    public static void main(String[] args) {
-        Random random = new Random();
 
-        Fazenda fazenda1[] = new Fazenda[50];
-        for (int i = 0; i < fazenda1.length; i++) {
-            // Criação de arrays do tipo Fazenda e cadastro de bovinos com valores aleatórios
-            fazenda1[i] = new Fazenda();
-            fazenda1[i].cadastrarBovino(random.nextInt(900000) + 100000, random.nextInt(340) + 160);
-        }
+	public static void main(String[] args) {
 
-        Fazenda fazenda2[] = new Fazenda[50];
-        int transferCount = 0;
+		Random random = new Random();
+		Scanner entrada = new Scanner(System.in);
+		Fazenda fazendaOrigem[] = new Fazenda[50];
+		Fazenda fazendaDestino[] = new Fazenda[50];
+		Truck caminhao[] = new Truck[40];
+		int viagem = 0;
+		int bovinosTransferidos = 0;
+		int escolha;
+		double media = 0;
+		double soma = 0;
 
-        Truck caminhao[] = new Truck[40];
+        do {
+    		
+            System.out.println("-------------------------------------------------");
+            System.out.println("          Sistema transporte de bovinos          ");
+            System.out.println("\n\t1 - Transporte de bovino");
+            System.out.println("\n\t2 - Receber em destino");
+            System.out.println("\n\t3 - Saldo");
+            System.out.println("\n\t0 - Sair");
+            System.out.print("\n\n\tEscolha uma opção: ");
+             
+            escolha = entrada.nextInt();
+            
 
-        while (!isFazenda1Empty(fazenda1)) {
-            // Transferência de bovinos de fazenda1 para fazenda2 com base na capacidade do caminhão
-            double soma = 0;
-            int bovinosTransferidos = 0;
+    		// Criação de arrays do tipo Fazenda e cadastro de bovinos com valores
+    		// aleatórios
+    		for (int i = 0; i < fazendaOrigem.length; i++) {
+    			fazendaOrigem[i] = new Fazenda(); // Inicializa um novo objeto na fazenda1 a cada interação
+    			fazendaOrigem[i].cadastrarBovino(random.nextInt(900000) + 100000, random.nextDouble(340) + 160);
+    			// System.out.println("FAZENDA ORIGEM - ID: " +
+    			// fazendaOrigem[i].getBovino().getId() + " - PESO: "
+    			// + fazendaOrigem[i].getBovino().getPeso());
+    		}
+    		// Declarando um novo array caminho para classe Truck
+    		// Inicializa um novo objeto no Truck a cada interação
+    		for (int j = 0; j < caminhao.length; j++) {
+    			caminhao[j] = new Truck();
+    		}            
+             
+            switch (escolha) { 
+                case 1:
+                	System.out.println("-------------------------------------------------");
+                    System.out.println("          Sistema transporte de bovinos          ");
+                    System.out.println("          Lista dos bovinos no caminhao            \n");
+                    
+//            		Transferência de bovinos de fazendaOrigem para caminhão
+            		for (var k = 0; k < caminhao.length; k++) {
+                        double resultado = 0;
+                        var carregamento = caminhao[viagem].getCarregamento();
+            			if (k == 0) {
+                            caminhao[viagem].transporte(fazendaOrigem[k].getBovino().getPeso());
+            				caminhao[k].cadTruckBovino(fazendaOrigem[k].getBovino().getId(),
+            						fazendaOrigem[k].getBovino().getPeso());
+            				fazendaOrigem[k] = null;
+            				bovinosTransferidos++;
+            			}
 
-            for (int j = 0; j < fazenda1.length; j++) {
-                if (fazenda1[j] != null) {
-                    soma += fazenda1[j].getBovino().getPeso();
-                    bovinosTransferidos++;
+            			// Calculo da média
+            			else if (k > 0 && carregamento > bovinosTransferidos) {
+            				for (var j = 0; j < bovinosTransferidos; j++) {
+                                if(caminhao[j].getBovino() != null){
+            					resultado += caminhao[j].getBovino().getPeso();
+            				}
+                        }
+            				media = (resultado + fazendaOrigem[k].getBovino().getPeso()) / (k + 1);
+            				caminhao[viagem].transporte(media);
 
-                    if (bovinosTransferidos <= caminhao.getCarregamento()) {
-                        caminhao[j] = fazenda1[j];
-                        fazenda2[j] = caminhao[j];
-                        caminhao[j] = null;
-                        fazenda1[j] = null;
+                            caminhao[k].cadTruckBovino(fazendaOrigem[k].getBovino().getId(),
+            						fazendaOrigem[k].getBovino().getPeso());
+            				fazendaOrigem[k] = null;
+            				bovinosTransferidos++;
+            			}
+
+            			else {
+                            for (var j = 0; j < bovinosTransferidos; j++) {
+                                if(caminhao[j].getBovino() != null){
+            					resultado += caminhao[j].getBovino().getPeso();
+            				}
+                        }
+                            caminhao[viagem].setCarga(resultado);
+                            caminhao[viagem].setViagem(viagem);
+            				viagem++;
+                            break;
+            			}
+            		}
+            		
+    				for (var j = 0; j < bovinosTransferidos; j++) {
+                        if(caminhao[j].getBovino() != null){
+    					System.out.println("   ID: " +caminhao[j].getBovino().getId() + " - Peso: " +  caminhao[j].getBovino().getPeso());
+    				}
+                }
+    				System.out.printf("\n   Carga: %.2f Kg", caminhao[viagem-1].getCarga());
+    				System.out.println("\n   Quantidade de Bovino: " + bovinosTransferidos);
+    				System.out.println("   Carregamento: "  + caminhao[viagem-1].getCarregamento());
+    				System.out.printf("   Media: %.2f Kg", media);
+    				System.out.println("\n-------------------------------------------------");
+                    
+    				for (var i = 0; i < bovinosTransferidos; i++) {
+    					if(caminhao[i].getBovino() != null){
+                        fazendaDestino[i] = new Fazenda();
+                        fazendaDestino[i].cadastrarBovino(caminhao[i].getBovino().getId(), caminhao[i].getBovino().getPeso());
                     }
                 }
-            }
-
-            System.out.println("Quantidade de vacas transferidas: " + bovinosTransferidos);
-
-            double media = soma / bovinosTransferidos;
-
-            caminhao.transporte(media);
-
-            transferCount++;
-        }
-
-        System.out.println("Quantidade de vezes que a transferência foi feita: " + transferCount);
-    }
-
-    private static boolean isFazenda1Empty(Fazenda[] fazenda) {
-        // Verifica se a fazenda1 está vazia, ou seja, se todos os elementos são nulos
-        for (Fazenda bovino : fazenda) {
-            if (bovino != null) {
-                return false;
-            }
-        }
-        return true;
-    }
+                break; 
+                case 2:                    
+                    System.out.println("-------------------------------------------------");
+                    System.out.println("          Sistema transporte de bovinos          \n");
+                    System.out.println("           Receber Fazenda de Destino            \n");
+                    System.out.println("\n   Quantidade de Bovino: " + bovinosTransferidos);
+                    System.out.printf("\n\n   Lista dos bovinos:\n\n");
+                    
+                    for (var i = 0; i < bovinosTransferidos; i++) {
+                        if (fazendaDestino[i] != null && fazendaDestino[i].getBovino() != null) {
+                            System.out.println("   ID: " + fazendaDestino[i].getBovino().getId() + " - Peso: " + fazendaDestino[i].getBovino().getPeso());
+                            caminhao[i] = null;
+                        }
+                    }
+                    
+                    
+                    System.out.println("\n-------------------------------------------------\n");
+                    System.out.println("\n\tBovinos recebidos com sucesso!!");
+                    System.out.println("\n-------------------------------------------------");
+                    break;
+                    
+                case 3: System.out.println("Opção 3 selecionada");
+                
+                break; 
+                case 0: System.out.println("Saindo...");
+                
+                break; 
+                default: System.out.println("Opção inválida. Tente novamente."); }
+            
+            }while (escolha != 0);
+	}
 }
